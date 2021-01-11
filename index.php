080ed0f1c764dc273124ba05e70209db3bbaa8e3
Korawich-sd <76939243+Korawich-sd@users.noreply.github.com>
2021-01-11 18:29:12 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>   
+             
+    <link rel="stylesheet" href="icon/css/all.css">
+
+    <link rel="stylesheet" href="css/mystyle.css">
+ <script src="js/default.js"></script> 
+</head>
+<body>
+    <div class="container">
+     hello wolee <br>
+    <img src="img/profile.jpg" width="200px" alt="" srcset=""> <hr>
+   
+        <button class="btn btn-primary"><i class="fas fa-ad"></i> กดดู </button>  <!-- แทรกicon-->
+        <button class="btn btn-primary"> <i class="fas fa-ad"></i>กดอีก </button>
+    </div>
+ 
+    <script>
+        $(()=>{
+            //alert("Hello");
+        });
+    </script>
+</body>
+</html>
